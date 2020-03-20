5f6fb32e46b2da43efcb5e9827cac0da2dceb3fe
Sabrina Knappe <sabrina.knappe@mail.mcgill.ca>
2020-03-19 21:35:38 -0400

    Update pyostart.py

--- mod/pyostart.py
+++ mod/pyostart.py
@@ -1,7 +1,8 @@
 import pyo
 
+snds_path = "audio"
 s = pyo.Server().boot()
 s.start()
-a = pyo.SfPlayer(SNDS_PATH+"audio/my_processed_audio_file.wav", loop=True, mul=0.7)
+a = pyo.SfPlayer(snds_path+"/my_processed_audio_file.wav", loop=True, mul=0.7)
 pva = pyo.PVAnal(a, size=1024, overlaps=4, wintype=2)
 pvs = pyo.PVSynth(pva).mix(2).out()
