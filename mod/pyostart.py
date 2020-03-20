3caa162e11221e99a98081e1a27bfa51ee1d1d20
Sabrina Knappe <sabrina.knappe@mail.mcgill.ca>
2020-03-19 21:27:23 -0400

    Update pyostart.py

--- mod/pyostart.py
+++ mod/pyostart.py
@@ -1,22 +1,7 @@
 import pyo
 
-# Creates a Server object with default arguments.
-# See the manual about how to change the sampling rate, the buffer
-# size, the number of channels or one of the other global settings.
-s = pyo.Server()
-
-# Boots the server. This step initializes audio and midi streams.
-# Audio and midi configurations (if any) must be done before that call.
-s.boot()
-
-# Starts the server. This step activates the server processing loop.
+s = pyo.Server().boot()
 s.start()
-
-# Here comes the processing chain...
-
-# The Server object provides a Graphical User Interface with the
-# gui() method. One of its purpose is to keep the program alive
-# while computing samples over time. If the locals dictionary is
-# given as argument, the user can continue to send commands to the
-# python interpreter from the GUI.
-s.gui(locals())
+a = pyo.SfPlayer(SNDS_PATH+"audio/my_processed_audio_file.wav", loop=True, mul=0.7)
+pva = pyo.PVAnal(a, size=1024, overlaps=4, wintype=2)
+pvs = pyo.PVSynth(pva).mix(2).out()
